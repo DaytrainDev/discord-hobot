8c43c71f001b03f28d000d94654ea1296f354b95
Guy <guy@Guys-MacBook-Pro.local>
2023-08-31 17:56:28 -0500

    health check test

--- src/Launcher.js
+++ src/Launcher.js
@@ -1,6 +1,19 @@
 import { config } from 'dotenv';
 import { Client } from './Client.js';
+import http from 'node:http';
 
 config({ path: '../.env' });
+
+const server = http.createServer((req, res) => {
+  // if (req.url === '/health') {
+  res.writeHead(200);
+  res.end('OK');
+  // } else {
+  //   res.writeHead(404);
+  //   res.end();
+  // }
+});
+
+server.listen(8080);
 
 new Client().start();
