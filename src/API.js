ccebcb7d8f6ccb03e7ce91890ede9dcb78bc2595
Guy <guy@Guys-MBP.midcoip.net>
2023-09-28 19:42:10 -0500

    build fixes

--- src/API.js
+++ src/API.js
@@ -1,6 +1,6 @@
 import http from 'node:http';
 
-export class API {
+export class Api {
   constructor(client) {
     this.client = client;
   }
@@ -9,13 +9,23 @@ export class API {
     const server = http.createServer((req, res) => {
       // TODO: build API for website
 
-      // if (req.url === '/health') {
-      res.writeHead(200);
-      res.end('OK');
-      // } else {
-      //   res.writeHead(404);
-      //   res.end();
-      // }
+      if (req.url === '/api/v0/') {
+        res.writeHead(200);
+
+        // TODO: process Api call with a service
+
+        res.end('OK');
+      } else if (req.url === '/') {
+        res.writeHead(200);
+        // TODO: show static html page
+        res.end('OK');
+      } else if (req.url === '/health') {
+        res.writeHead(200);
+        res.end('OK');
+      } else {
+        res.writeHead(404);
+        res.end();
+      }
     });
     server.listen(8080);
   }
--- src/Launcher.js
+++ src/Launcher.js
@@ -1,8 +1,8 @@
 import { config } from 'dotenv';
 import { Client } from './Client.js';
-import { API } from './API.js';
+import { Api } from './Api.js';
 
 config({ path: '../.env' });
 
 const client = await new Client().start();
-new API(client).start();
+new Api(client).start();
