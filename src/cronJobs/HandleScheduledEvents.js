d777de65517bc52dafe6569f7ef38144ff7c988c
Guy <the0bob@users.noreply.github.com>
2023-11-18 11:05:51 -0600

    disable event auto-start since Discord supports this natively now

--- src/cronJobs/HandleScheduledEvents.js
+++ src/cronJobs/HandleScheduledEvents.js
@@ -1,44 +1,46 @@
-import { CronJob } from '../structures/CronJob.js';
-import { GuildScheduledEventStatus } from 'discord.js';
+// import { CronJob } from '../structures/CronJob.js';
+// import { GuildScheduledEventStatus } from 'discord.js';
 
-const MINUTE = 1000 * 60;
+// const MINUTE = 1000 * 60;
+//
+// Discord now has an option tio autostart events
+//
+// export default class HandleScheduledEvents extends CronJob {
+//   constructor() {
+//     super();
+//     this.schedule = '* * * * *';
+//   }
 
-export default class HandleScheduledEvents extends CronJob {
-  constructor() {
-    super();
-    this.schedule = '* * * * *';
-  }
+//   async execute(client) {
+//     await this.startEvents(client);
+//     await this.endEvents(client);
+//   }
 
-  async execute(client) {
-    await this.startEvents(client);
-    await this.endEvents(client);
-  }
+//   async startEvents(client) {
+//     const guilds = await client.guilds.cache;
+//     guilds?.forEach(async guild => {
+//       const allScheduledEvents = await guild?.scheduledEvents?.fetch();
+//       const startingScheduledEvents = allScheduledEvents
+//         ?.filter(ev => MINUTE > (new Date(ev.scheduledStartTimestamp).getTime() - Date.now()));
+//       startingScheduledEvents
+//         ?.forEach((ev) => ev.setStatus(GuildScheduledEventStatus.Active));
+//     });
+//   }
 
-  async startEvents(client) {
-    const guilds = await client.guilds.cache;
-    guilds?.forEach(async guild => {
-      const allScheduledEvents = await guild?.scheduledEvents?.fetch();
-      const startingScheduledEvents = allScheduledEvents
-        ?.filter(ev => MINUTE > (new Date(ev.scheduledStartTimestamp).getTime() - Date.now()));
-      startingScheduledEvents
-        ?.forEach((ev) => ev.setStatus(GuildScheduledEventStatus.Active));
-    });
-  }
+//   async endEvents(client) {
+//     const guilds = await client.guilds.cache;
 
-  async endEvents(client) {
-    const guilds = await client.guilds.cache;
-
-    guilds?.forEach(async guild => {
-      const allCurrentEvents = await guild?.scheduledEvents?.fetch();
-      const activeEvents = allCurrentEvents
-        ?.filter(ev => ev.status === GuildScheduledEventStatus.Active);
-      const emptyActiveEvents = activeEvents
-        ?.filter(ev => ev.voiceChannel.members.size === 0);
-      emptyActiveEvents
-        ?.forEach((ev) => ev.setStatus(GuildScheduledEventStatus.Completed));
-      // filer down to active events
-      // filter down to events that are in empty voice channels
-      // stop the filtered events
-    });
-  }
-}
+//     guilds?.forEach(async guild => {
+//       const allCurrentEvents = await guild?.scheduledEvents?.fetch();
+//       const activeEvents = allCurrentEvents
+//         ?.filter(ev => ev.status === GuildScheduledEventStatus.Active);
+//       const emptyActiveEvents = activeEvents
+//         ?.filter(ev => ev.voiceChannel.members.size === 0);
+//       emptyActiveEvents
+//         ?.forEach((ev) => ev.setStatus(GuildScheduledEventStatus.Completed));
+//       // filer down to active events
+//       // filter down to events that are in empty voice channels
+//       // stop the filtered events
+//     });
+//   }
+// }
